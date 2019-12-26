e090a1b65ce4153cfe7df8feda23277debcc8456
Shital Patel <shital732@yahoo.com>
2019-12-26 17:15:47 -0500

    add income and expense type in budgetController

--- budget.js
+++ budget.js
@@ -16,6 +16,33 @@ var budgetController = (function() {
       inc: 0
     }
   };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      //Creat new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      // create a new item based on 'inc or 'exp' type
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+      // push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // return the new element
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
 })();
 
 var UIcontroller = (function() {
@@ -43,6 +70,7 @@ var UIcontroller = (function() {
 var controller = (function(budgetctrl, UIctrl) {
   var setupEventListners = function() {
     var DOM = UIctrl.getDOMstrings();
+
     document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function(event) {
@@ -52,7 +80,21 @@ var controller = (function(budgetctrl, UIctrl) {
     });
   };
   var ctrlAddItem = function() {
+    // Get the field input data
     var input = UIctrl.getInput();
+
+    // Add the item to the budget controller
+    var newItem = budgetController.addItem(
+      input.type,
+      input.discription,
+      input.value
+    );
+
+    // Add the item to the UI
+
+    // Calculate the budget
+
+    // Display the budget on the UI
   };
 
   return {
